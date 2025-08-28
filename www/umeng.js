8fa06505b182ac206761718bf24acf3075743afa
boboxiaodd <iam386@gmail.com>
2025-08-28 14:45:52 +0800

    Update umeng.js
    add uid when crash

--- www/umeng.js
+++ www/umeng.js
@@ -2,6 +2,9 @@ const exec = require('cordova/exec');
 const CDVUMeng = {
     getUMID:function (success){
         exec(success,null,'CDVUMeng','getUMID',[]);
-    }
+    },
+    setUId:function (option){
+        exec(null,null,'CDVUMeng','setUId',[option]);
+    },
 };
 module.exports = CDVUMeng;
